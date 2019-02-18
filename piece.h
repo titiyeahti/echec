2f6c08aef2dc258a7fa6057e7b07bb734b5ded1e
Thibaut MILHAUD <thibaut.milhaud@gmail.com>
2019-02-18 19:18:13 +0100

    rework de debut de projet

--- piece.h
+++ piece.h
@@ -1,20 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-typedef enum typepiece {
-		VIDE,
-		PION,
-		CAVALIER,
-		FOU,
-		TOUR,
-		DAME,
-		ROI
-} typepiece;
+#define VIDE 0
+#define PION2CASE 1
+#define PION 2
+#define TOUR 3
+#define CAVA 4
+#define FOU 5
+#define DAME 6
+#define ROI 7
 
 /* c contient l'info de la couleur (0 ou 1, 0 pour les noirs évidement) */
-typedef struct piece {
-		typepiece t;
-		char c;
-}
 
-piece nouvelle_piece(typepiece t, char c);
+char nouvelle_piece(char t, char c);
